0cce796b3f86290d097183d4cb02062f95be7199
Matthew Landauer <mlandauer@gmail.com>
2019-06-07 17:18:43 +1000

    Upgrade to latest gem

--- scraper.rb
+++ scraper.rb
@@ -5,7 +5,8 @@ agent.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
 IconScraper.rest_xml(
   "https://planningexchange.coffsharbour.nsw.gov.au/PortalProd/Pages/XC.Track/SearchApplication.aspx",
-  "d=thisweek&k=LodgementDate&o=xml",
-  false,
+  {d: "thisweek", k: "LodgementDate", o: "xml"},
   agent
-)
+) do |record|
+  IconScraper.save(record)
+end
