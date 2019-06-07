6f5a9268f5148f8c16846b25b71251a377a4e764
Matthew Landauer <mlandauer@gmail.com>
2019-06-07 17:17:15 +1000

    Get rid of MORPH_PERIOD for the time being

--- scraper.rb
+++ scraper.rb
@@ -1,21 +1,11 @@
 require "icon_scraper"
 
-case ENV['MORPH_PERIOD']
-  when 'lastmonth'
-  	period = "lastmonth"
-  when 'thismonth'
-  	period = "thismonth"
-  else
-    period = "thisweek"
-end
-puts "Getting data in `" + period + "`, changable via MORPH_PERIOD environment"
-
 agent = Mechanize.new
 agent.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
 IconScraper.rest_xml(
   "https://planningexchange.coffsharbour.nsw.gov.au/PortalProd/Pages/XC.Track/SearchApplication.aspx",
-  "d=#{period}&k=LodgementDate&o=xml",
+  "d=thisweek&k=LodgementDate&o=xml",
   false,
   agent
 )
