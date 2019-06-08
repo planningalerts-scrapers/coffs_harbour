5c4bdb81e2ae5f3022aa9a65b58918bcabcc964c
Matthew Landauer <mlandauer@gmail.com>
2019-06-08 11:30:02 +1000

    Update to latest version of gem

--- scraper.rb
+++ scraper.rb
@@ -1,12 +1,3 @@
 require "icon_scraper"
 
-agent = Mechanize.new
-agent.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-IconScraper.rest_xml(
-  "https://planningexchange.coffsharbour.nsw.gov.au/PortalProd/Pages/XC.Track/SearchApplication.aspx",
-  {d: "thisweek", k: "LodgementDate", o: "xml"},
-  agent
-) do |record|
-  IconScraper.save(record)
-end
+IconScraper.scrape_and_save(:coffs_harbour)
