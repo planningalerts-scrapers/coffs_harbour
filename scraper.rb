7dba790a3ac9be37f1a48db36e9094cb09ae982a
Matthew Landauer <mlandauer@gmail.com>
2019-06-12 06:02:09 +1000

    Use latest version of the gem

--- scraper.rb
+++ scraper.rb
@@ -1,12 +1,3 @@
 require "icon_scraper"
 
-# Testing to figure out source of 502 bad gateway on morph (but not when run locally)
-agent = Mechanize.new
-agent.verify_mode = OpenSSL::SSL::VERIFY_NONE
-# agent.set_proxy("36.91.129.164", 3128)
-
-page = agent.get("https://planningexchange.coffsharbour.nsw.gov.au/PortalProd/Pages/XC.Track/SearchApplication.aspx?d=thisweek&k=LodgementDate&o=xml")
-puts page.body
-exit
-
 IconScraper.scrape_and_save(:coffs_harbour)
