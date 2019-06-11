4430b59ad69891baef687cb08c96a68e8e55eaa4
Matthew Landauer <mlandauer@gmail.com>
2019-06-12 05:55:30 +1000

    Disable proxy

--- scraper.rb
+++ scraper.rb
@@ -3,7 +3,7 @@ require "icon_scraper"
 # Testing to figure out source of 502 bad gateway on morph (but not when run locally)
 agent = Mechanize.new
 agent.verify_mode = OpenSSL::SSL::VERIFY_NONE
-agent.set_proxy("36.91.129.164", 3128)
+# agent.set_proxy("36.91.129.164", 3128)
 
 page = agent.get("https://planningexchange.coffsharbour.nsw.gov.au/PortalProd/Pages/XC.Track/SearchApplication.aspx?d=thisweek&k=LodgementDate&o=xml")
 puts page.body
